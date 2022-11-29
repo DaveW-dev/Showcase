b17b558a8373bd973f9f57d5d0bbb58c83349945
DaveW <davewdev@proton.me>
2022-11-29 14:32:13 +0000

    1
    1

---
+++ Assets/Scripts/PlayerStates/PlayerFallState.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerFallState : PlayerBaseState
+{
+    private readonly int FallHash = Animator.StringToHash("Fall");
+    private const float CrossFadeDuration = 0.5f;
+
+    public PlayerFallState(PlayerStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter()
+    {
+        stateMachine.Velocity.y = 0f;
+
+        stateMachine.Animator.CrossFadeInFixedTime(FallHash, CrossFadeDuration);
+    }
+
+    public override void Tick()
+    {
+        ApplyGravity();
+        CalculateMoveDirection();
+        FaceMoveDirection();
+        Move();
+
+        if (stateMachine.Controller.isGrounded)
+        {
+            stateMachine.SwitchState(new PlayerMoveState(stateMachine));
+        }
+    }
+
+    public override void Exit() { }
+}
